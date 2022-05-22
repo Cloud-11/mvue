ada3564ad9fa2998da8ffcb439c7ada1e54f0e8e
cloud11 <mhbjxin@gmail.com>
2022-05-22 23:40:03 +0800

    增加公共方法

--- packages/shard/src/index.ts
+++ packages/shard/src/index.ts
@@ -3,7 +3,8 @@
  * @param obj
  * @returns
  */
-const isObject = (obj: any) => {
-  return typeof obj === "object" && obj !== null;
-};
-export default isObject;
+export const isArray = Array.isArray;
+export const isFunction = (val: any) => typeof val === "function";
+export const isString = (val: any) => typeof val === "string";
+export const isSymbol = (val: any) => typeof val === "symbol";
+export const isObject = (val: any) => val !== null && typeof val === "object";
