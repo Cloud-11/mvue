a03ffab4483e26ec77cfda368df5708efa9d859d
cloud11 <mhbjxin@gmail.com>
2022-05-30 16:04:40 +0800

    watch onCleanup

--- packages/reactivity/src/watch.ts
+++ packages/reactivity/src/watch.ts
@@ -13,7 +13,7 @@ const traversal = (val: any, set = new Set()) => {
 };
 export const watch = (
   source: () => void | object,
-  callback: (oldval: any, newVal: any) => void
+  callback: (oldval: any, newVal: any, onCleanup: (fn: () => any) => void) => void
 ) => {
   let getter;
   if (isReactive(source)) {
@@ -27,13 +27,32 @@ export const watch = (
     return;
   }
 
+  //保存旧值
   let oldVal: any;
+
+  //watch的oncleanup解决的是连续请求异步数据，数据返回会一直变。
+  //cleanup 可以清除(跳过,不使用)之前的返回的数据，数据变化一步到位。
+
+  //保存清除函数
+  let cleanUp: () => any;
+  //触发清除函数
+  //watch里调用异步函数，http请求等，可以在持续触发状态，清理上次的请求
+  const onCleanUp = (fn: () => any) => {
+    cleanUp = fn;
+  };
+
+  //effect scheduler函数，调度函数。下次触发,优先执行
+  //第一次调用，cleanUp才赋值。
   const cb = () => {
+    //先执行清除
+    if (cleanUp) cleanUp();
     const newVal = effect.run();
-    callback(oldVal, newVal);
+    callback(oldVal, newVal, onCleanUp);
     oldVal = newVal;
   };
+
   const effect = new ReactEffect(<() => any>getter, cb);
+
   //执行getter  触发收集
   oldVal = effect.run();
 };
