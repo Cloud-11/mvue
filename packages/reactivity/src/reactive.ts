2ac53e85e93ee3f7fcfe1dc1352f48052091117f
cloud11 <mhbjxin@gmail.com>
2022-05-31 23:45:26 +0800

    readonly

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -12,10 +12,11 @@ export function reactive(target: any) {
   //防止代理源对象为代理过的,二次代理
   //源对象可以是用户自己的proxy
   //添加代理标志
-
+  //否是代理对象
   if (target[ReactiveFlags.IS_REACTIVE]) {
     return target;
   }
+  //源对象是否被代理过
   if (reactiveMap.get(target)) {
     return reactiveMap.get(target);
   }
@@ -23,4 +24,18 @@ export function reactive(target: any) {
   const proxy = new Proxy(target, mutableHandles);
   reactiveMap.set(target, proxy);
   return proxy;
+}
+
+// 生成readonly 响应式数据
+export function readonly(raw: any) {
+  return new Proxy(raw, {
+    get(target, key) {
+      return Reflect.get(target, key);
+    },
+
+    set(target, key, value) {
+      console.warn(`key:${String(key)} set 失败 因为 target 是 readonly`);
+      return true;
+    },
+  });
 }
