35c70884f9de9804403a73b316d97908c940953b
cloud11 <mhbjxin@gmail.com>
2022-06-01 00:30:05 +0800

    test

--- test/reactive.spec.js
+++ test/reactive.spec.js
@@ -1,4 +1,4 @@
-import { readonly } from "@mvue/reactivity";
+import { readonly, ref } from "@mvue/reactivity";
 import { jest } from "@jest/globals";
 
 describe("readonly test", () => {
@@ -16,4 +16,23 @@ describe("readonly test", () => {
   });
 });
 
-// readonly.spec.ts
+describe("ref test", () => {
+  it("ref代理对象", () => {
+    const original = { foo: 1, bar: { baz: 2 } };
+    const wrapped = ref(original);
+    expect(wrapped).not.toBe(original);
+    expect(wrapped.value.foo).toBe(1);
+  });
+  it("ref代理number", () => {
+    const wrapped = ref(1);
+    expect(wrapped.value).toBe(1);
+  });
+  it("ref代理string", () => {
+    const wrapped = ref("1");
+    expect(wrapped.value).toBe("1");
+  });
+  it("ref代理boolean", () => {
+    const wrapped = ref(true);
+    expect(wrapped.value).toBe(true);
+  });
+});
